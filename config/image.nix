e8cdb05dd507df7236e368f4b8a2208f780247cd
niksingh710 <nik.singh710@gmail.com>
2024-06-17 13:28:58 +0530

    Fixed the w/h size in image nvim

--- config/image.nix
+++ config/image.nix
@@ -3,6 +3,8 @@
     enable = true;
     backend = "ueberzug";
     editorOnlyRenderWhenFocused = true;
+    maxWidth = 50;
+    maxHeight = 50;
     integrations.markdown.onlyRenderImageAtCursor = true;
   };
 }
