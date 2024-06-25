30824a1c1700b8d43b4a8f09e718a79043103f8e
niksingh710 <nik.singh710@gmail.com>
2024-06-25 07:24:42 +0530

    Added otter in cmp config
    will try later if possible to add it in config/otter.nix

--- config/cmp.nix
+++ config/cmp.nix
@@ -25,6 +25,7 @@
             '';
         };
         sources = [
+          { name = "otter"; }
           { name = "async_path"; }
           { name = "nvim_lsp_signature_help"; }
           {
@@ -44,77 +45,77 @@
         mapping = {
           __raw = # lua
             ''
-            cmp.mapping.preset.insert({
-              ["<C-k>"] = cmp.mapping.select_prev_item(),
-              ["<C-j>"] = cmp.mapping.select_next_item(),
+              cmp.mapping.preset.insert({
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                ["<C-j>"] = cmp.mapping.select_next_item(),
 
-              ["<c-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-              ["<c-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+                ["<c-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+                ["<c-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 
-              ["<C-e>"] = cmp.mapping({
-                i = cmp.mapping.abort(),
-                c = cmp.mapping.close(),
-              }),
+                ["<C-e>"] = cmp.mapping({
+                  i = cmp.mapping.abort(),
+                  c = cmp.mapping.close(),
+                }),
 
-              -- Making Ctrl-Enter accept the top entry instead of Enter
-              ["<c-CR>"] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Insert,
-                select = true,
-              }),
-              ["<CR>"] = cmp.mapping({
-                i = function(fallback)
-                  fallback()
-                end,
-              }),
+                -- Making Ctrl-Enter accept the top entry instead of Enter
+                ["<c-CR>"] = cmp.mapping.confirm({
+                  behavior = cmp.ConfirmBehavior.Insert,
+                  select = true,
+                }),
+                ["<CR>"] = cmp.mapping({
+                  i = function(fallback)
+                    fallback()
+                  end,
+                }),
 
-              ["<c-l>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif lsnip.expand_or_jumpable() then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                ["<c-l>"] = cmp.mapping(function(fallback)
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif lsnip.expand_or_jumpable() then
+                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                  else
+                    fallback()
+                  end
+                end, { "i", "s" }),
+                ["<c-h>"] = cmp.mapping(function(fallback)
+                  if cmp.visible() then
+                    cmp.select_prev_item()
+                  elseif lsnip.jumpable(-1) then
+                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                  else
+                    fallback()
+                  end
+                end, { "i", "s" }),
+                ["<c-space>"] = cmp.mapping(function(fallback)
+
+                local copk, _ = pcall(require, "copilot")
+
+                if copk then
+                  vim.g.copilot_no_tab_map = true
+                  vim.g.copilot_assume_mapped = true
+                  vim.g.copilot_tab_fallback = ""
+                  local suggestion = require("copilot.suggestion")
+                  if suggestion.is_visible() then
+                    suggestion.accept()
+                  elseif cmp.visible() then
+                    cmp.select_next_item()
+                  elseif require("luasnip").expand_or_jumpable() then
+                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                  else
+                    fallback()
+                  end
                 else
-                  fallback()
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif require("luasnip").expand_or_jumpable() then
+                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                  else
+                    fallback()
+                  end
                 end
-              end, { "i", "s" }),
-              ["<c-h>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif lsnip.jumpable(-1) then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                else
-                  fallback()
-                end
-              end, { "i", "s" }),
-              ["<c-space>"] = cmp.mapping(function(fallback)
-
-              local copk, _ = pcall(require, "copilot")
-
-              if copk then
-                vim.g.copilot_no_tab_map = true
-                vim.g.copilot_assume_mapped = true
-                vim.g.copilot_tab_fallback = ""
-                local suggestion = require("copilot.suggestion")
-                if suggestion.is_visible() then
-                  suggestion.accept()
-                elseif cmp.visible() then
-                  cmp.select_next_item()
-                elseif require("luasnip").expand_or_jumpable() then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                else
-                  fallback()
-                end
-              else
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif require("luasnip").expand_or_jumpable() then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                else
-                  fallback()
-                end
-              end
-              end, { "i", "s" })
-             })
-          '';
+                end, { "i", "s" })
+               })
+            '';
         };
       };
     };
