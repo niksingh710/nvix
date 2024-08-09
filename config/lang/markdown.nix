6d03548393dba5443a90a23e871334d783786b39
Nikhil Singh <nik.singh710@gmail.com>
2024-08-09 11:34:50 +0530

    Added markview per line preview

--- config/lang/markdown.nix
+++ config/lang/markdown.nix
@@ -24,6 +24,23 @@ in {
   ];
   extraConfigLua = # lua
     ''
+      local ok, mv = pcall(require, "markview")
+      if ok then
+        mv.setup({
+          modes = { "n", "c" },
+          hybrid_modes = { "n" },
+
+          callbacks = {
+            on_enable = function (_, win)
+              vim.wo[win].conceallevel = 2;
+              -- This will prevent Tree-sitter concealment being disabled on the cmdline mode
+              vim.wo[win].concealcursor = "c";
+            end
+          }
+        })
+      end
+
+
       local ok, md = pcall(require, "hl-mdcodeblock")
       if ok then
         md.setup()
