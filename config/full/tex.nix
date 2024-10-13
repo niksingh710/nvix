2508a638f02f39f76035e09becf7a650e55778ce
niksingh710 <nik.singh710@gmail.com>
2024-10-13 12:51:13 +0530

    need dirty tex

--- config/full/tex.nix
+++ config/full/tex.nix
@@ -19,13 +19,6 @@
     (mkPkgs "rnoweb-nvim" inputs.rnoweb)
   ];
 
-  extraConfigLuaPre = # lua
-    ''
-      vim.g.vimtex_compiler_latexmk = {
-        aux_dir = ".build" -- you can set here whatever name you desire
-      }
-    '';
-
   extraConfigLua = # lua
     ''
       require('rnoweb-nvim').setup()
