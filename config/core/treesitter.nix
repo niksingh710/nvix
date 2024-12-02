25377e1dc5fe1b921df3814da753d259f7bdbcee
niksingh710 <nik.singh710@gmail.com>
2024-12-03 01:08:05 +0530

    treesitter: Fixes large file loading time

--- config/core/treesitter.nix
+++ config/core/treesitter.nix
@@ -1,3 +1,4 @@
+{ helpers, ... }:
 {
   plugins = {
     treesitter = {
@@ -5,9 +6,18 @@
       settings = {
         highlight = {
           enable = true;
-          disable = [
-            "latex"
-          ];
+          disable = helpers.mkRaw ''
+            function(lang, buf)
+              if lang == 'latex' then
+                return true
+              end
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+              if ok and stats and stats.size > max_filesize then
+                  return true
+              end
+            end
+          '';
         };
         incremental_selection.enable = true;
         indent.enable = true;
@@ -29,4 +39,3 @@
     })
   '';
 }
-
