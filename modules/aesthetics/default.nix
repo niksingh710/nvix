2a5e805b0a365e2588eb86946b2e9bcba5a837cd
niksingh710 <nik.singh710@gmail.com>
2025-01-31 13:42:44 +0530

    aesthetics: Added Colorizer for all filetypes
    Added minty for Now, Only Invokable by command

--- modules/aesthetics/default.nix
+++ modules/aesthetics/default.nix
@@ -1,18 +1,49 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   extraPlugins = [
     pkgs.vimPlugins.windows-nvim
+    pkgs.vimPlugins.nvzone-minty
   ];
-
   extraConfigLua = # lua
     ''
       vim.o.winwidth = 10
       vim.o.winminwidth = 10
       vim.o.equalalways = false
       require('windows').setup()
+
+      require('minty').setup({
+        huefy = {
+          border = "${config.nvix.border}",
+          mappings = function(buf)
+            local api = require("minty.shades.api")
+            vim.keymap.set("n", "s", api.save_color, { buffer = buf })
+          end
+        }
+      })
     '';
 
   plugins = {
+    colorizer = {
+      enable = true;
+      settings = {
+        filetypes = {
+          __unkeyed = "*";
+        };
+        user_default_options = {
+          names = true;
+          RRGGBBAA = true;
+          AARRGGBB = true;
+          rgb_fn = true;
+          hsl_fn = true;
+          css = true;
+          css_fn = true;
+          tailwind = true;
+          mode = "virtualtext";
+          virtualtext = "■";
+          always_update = true;
+        };
+      };
+    };
     dressing = {
       enable = true;
       settings.input.mappings.n = {
@@ -25,7 +56,7 @@
     fidget = {
       enable = true;
       settings = {
-        progress.display.progressIcon.pattern = "moon";
+        progress.display.progress_icon = [ "moon" ];
         notification.window = {
           relative = "editor";
           winblend = 0;
