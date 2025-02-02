7016cd84606953f43707e499b90cfad28777e467
niksingh710 <nik.singh710@gmail.com>
2025-02-02 23:38:21 +0530

    desc: fixes meta values for nix builds

--- default.nix
+++ default.nix
@@ -10,23 +10,31 @@
         inherit pkgs extraSpecialArgs;
         module = import ./config/${type}.nix;
       };
-      check = nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule;
-      package = nixvim.legacyPackages.${system}.makeNixvimWithModule;
+      check = attr: description:
+        let
+          output = nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule attr;
+        in
+        output // { meta = output.meta // { inherit description; }; };
+      package = attr: description:
+        let
+          output = nixvim.legacyPackages.${system}.makeNixvimWithModule attr;
+        in
+        output // { meta = output.meta // { inherit description; }; };
     in
     {
       # Run `nix flake check .` to verify that your config is not broken
       checks = rec {
-        default = check (nvix "core") // { meta.description = "Checks if default builds fine."; };
-        core = default // { meta.description = "Checks if core builds fine."; };
-        bare = check (nvix "bare") // { meta.description = "Checks if bare builds fine"; };
-        full = check (nvix "full") // { meta.description = "Checks if full builds fine"; };
+        default = check (nvix "core") "Checks if core builds fine.";
+        core = default;
+        bare = check (nvix "bare") "Checks if bare builds fine";
+        full = check (nvix "full") "Checks if full builds fine";
       };
       # Lets you run `nix run .` to start nixvim
       packages = rec {
-        default = package (nvix "core") // { meta.description = "-> core"; };
-        core = default // { meta.description = "Balanced for regular usage"; };
-        bare = package (nvix "bare") // { meta.description = "Minimal for servers/quick spin."; };
-        full = package (nvix "full") // { meta.description = "Full utility set with tex support."; };
+        default = package (nvix "core") "Balanced for regular usage";
+        core = default;
+        bare = package (nvix "bare") "Minimal utility set (Good for servers to quick spin)";
+        full = package (nvix "full") "Full utility set (Full Set contains tex utilities)";
       };
       formatter = pkgs.nixpkgs-fmt;
     };
