7f9ad9fb2b6f22bc99cb3968faa723ef05e04507
niksingh710 <nik.singh710@gmail.com>
2025-02-17 15:58:02 +0530

    remove: mini.files removed in favour of keeping oil.nvim and neo-tree

--- modules/explorer/mini.files/keymaps.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, ... }:
-let inherit (config.nvix.mkKey) mkKeymap;
-in {
-  keymaps = lib.optionals config.nvix.explorer.mini [
-    (mkKeymap "n" "<leader>e" "<cmd>:lua MiniFiles.open()<cr>" "Mini Explorer")
-  ];
-
-}
