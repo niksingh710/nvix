5cb27a4da439afc53790951df2749daad4ede0dd
niksingh710 <nik.singh710@gmail.com>
2025-05-23 09:33:33 +0530

    chore(plugin): Added kulala plugin for web request testing.
    This will enable keymaps in [http, rest] file extensions
    e.g `file.http or file.rest`

--- modules/lang/webdev.nix
+++ modules/lang/webdev.nix
@@ -1,5 +1,37 @@
+{ config, ... }:
+
+let
+  inherit (config.nvix.mkKey) wKeyObj;
+in
 {
+  wKeyList = [
+    (wKeyObj [
+      "<leader>R"
+      "󱞒"
+      "Http Requests"
+    ])
+  ];
   plugins = {
+    kulala = {
+      enable = true; # for rest api (alter to insomnia and postman)
+      luaConfig.post = # lua
+        ''
+          require("kulala").setup({
+            global_keymaps = true;
+            global_keymaps_prefix = "<leader>R";
+            kulala_keymaps_prefix = "";
+            })
+        '';
+      lazyLoad = {
+        enable = true;
+        settings = {
+          ft = [
+            "http"
+            "rest"
+          ];
+        };
+      };
+    };
     ts-autotag.enable = true;
     ts-comments.enable = true;
     lsp.servers = {
