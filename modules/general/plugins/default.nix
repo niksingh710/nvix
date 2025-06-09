24519a5a57280bc12cc2738dc205531ceffad41a
niksingh710 <nik.singh710@gmail.com>
2025-06-09 18:28:25 +0530

    chore(update): Flake is udpated.

--- modules/general/plugins/default.nix
+++ modules/general/plugins/default.nix
@@ -12,7 +12,10 @@
     smart-splits.enable = true;
     web-devicons.enable = true;
     lz-n.enable = true;
-    flash.enable = true;
+    flash = {
+      enable = true;
+      settings.modes.char.enabled = false;
+    };
     mini = {
       enable = true;
       mockDevIcons = true;
