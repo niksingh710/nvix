5499232dc66c628f985b9616dcfd41706dec739d
LuposX <schuppsimon5gmail.com>
2025-06-13 10:40:45 +0200

    added toggleterm
    added toggleterm
    
    added toggleterm
    
    added toggleterm

--- modules/toggleterm/default.nix
+++ modules/toggleterm/default.nix
@@ -1,45 +1,52 @@
-{ ... }: {
+_: {
   plugins.toggleterm = {
     enable = true;
-     settings = {
-        hide_numbers = false;
-        autochdir = true;
-        close_on_exit = true;
-        direction = "vertical";
-      };
-      keymaps = [
-        {
-          mode = "n";
-          key = "<leader>t";
-          action = "<cmd>ToggleTerm<cr>";
-          options = {
-            desc = "Toggle Terminal Window";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>tv";
-          action = "<cmd>ToggleTerm direction=vertical<cr>";
-          options = {
-            desc = "Toggle Vertical Terminal Window";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>th";
-          action = "<cmd>ToggleTerm direction=horizontal<cr>";
-          options = {
-            desc = "Toggle Horizontal Terminal Window";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>tf";
-          action = "<cmd>ToggleTerm direction=float<cr>";
-          options = {
-            desc = "Toggle Floating Terminal Window";
-          };
-        }
-      ];
+    settings = {
+      autochdir = true;
+      close_on_exit = true;
+      direction = "vertical";
     };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>t";
+      action = "<cmd>ToggleTerm<cr>";
+      options = {
+        desc = "Toggle Terminal Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tv";
+      action = "<cmd>ToggleTerm direction=vertical<cr>";
+      options = {
+        desc = "Toggle Vertical Terminal Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>th";
+      action = "<cmd>ToggleTerm direction=horizontal<cr>";
+      options = {
+        desc = "Toggle Horizontal Terminal Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tf";
+      action = "<cmd>ToggleTerm direction=float<cr>";
+      options = {
+        desc = "Toggle Floating Terminal Window";
+      };
+    }
+    {
+      mode = "t";
+      key = "<Esc>";
+      action = "<C-\\><C-n>";
+      options = {
+        desc = "Exit terminal mode";
+      };
+    }
+  ];
 }
