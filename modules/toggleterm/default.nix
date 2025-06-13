f96a03bc1e908a9867da036c8866c2aaf8ed1baa
LuposX <schuppsimon5gmail.com>
2025-06-13 10:42:34 +0200

    added toggleterm

--- modules/toggleterm/default.nix
+++ modules/toggleterm/default.nix
@@ -7,39 +7,39 @@
         close_on_exit = true;
         direction = "vertical";
       };
-      keymaps = [
-        {
-          mode = "n";
-          key = "<leader>t";
-          action = "<cmd>ToggleTerm<cr>";
-          options = {
-            desc = "Toggle Terminal Window";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>tv";
-          action = "<cmd>ToggleTerm direction=vertical<cr>";
-          options = {
-            desc = "Toggle Vertical Terminal Window";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>th";
-          action = "<cmd>ToggleTerm direction=horizontal<cr>";
-          options = {
-            desc = "Toggle Horizontal Terminal Window";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>tf";
-          action = "<cmd>ToggleTerm direction=float<cr>";
-          options = {
-            desc = "Toggle Floating Terminal Window";
-          };
-        }
-      ];
-    };
+    }
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>t";
+        action = "<cmd>ToggleTerm<cr>";
+        options = {
+          desc = "Toggle Terminal Window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>tv";
+        action = "<cmd>ToggleTerm direction=vertical<cr>";
+        options = {
+          desc = "Toggle Vertical Terminal Window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>th";
+        action = "<cmd>ToggleTerm direction=horizontal<cr>";
+        options = {
+          desc = "Toggle Horizontal Terminal Window";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>tf";
+        action = "<cmd>ToggleTerm direction=float<cr>";
+        options = {
+          desc = "Toggle Floating Terminal Window";
+        };
+      }
+  ];
 }
