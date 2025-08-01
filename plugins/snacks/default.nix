2543b3fefd74ce14d16bca2736859e0f106c8c4a
Nikhil Singh <nik.singh710@gmail.com>
2025-08-01 21:36:54 +0530

    lazygit: fix tab opening and edit flake

--- plugins/snacks/default.nix
+++ plugins/snacks/default.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ... }:
 let
-  inherit (lib.nixvim) utils;
+  inherit (lib.nixvim) utils mkRaw;
 in
 {
   # Generalise for all colorschemes
@@ -16,6 +16,10 @@ in
     settings = {
       bigfile.enabled = true;
       scroll.enabled = false;
+      lazygit.config.os.edit = mkRaw # lua
+        ''
+          '[ -z "\"$NVIM\"" ] && (nvim -- {{filename}}) || (nvim --server "\"$NVIM\"" --remote-send "\"q\"" && nvim --server "\"$NVIM\"" --remote {{filename}})'
+        '';
       quickfile.enabled = true;
       indent.enabled = true;
       words.enabled = true;
@@ -74,30 +78,31 @@ in
           ''
             -- Taken from https://github.com/folke/snacks.nvim?tab=readme-ov-file#-usage
             function()
-              -- Setup some globals for debugging (lazy-loaded)
-              _G.dd = function(...)
-                Snacks.debug.inspect(...)
+            -- Setup some globals for debugging (lazy-loaded)
+            _G.dd = function (...)
+              Snacks.debug.inspect
+              (...)
               end
               _G.bt = function()
-                Snacks.debug.backtrace()
-              end
-              vim.print = _G.dd -- Override print to use snacks for `:=` command
+            Snacks.debug.backtrace()
+            end
+            vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-              -- Create some toggle mappings
-              Snacks.toggle.diagnostics():map("<leader>ud")
-              Snacks.toggle.line_number():map("<leader>ul")
-              Snacks.toggle.inlay_hints():map("<leader>uh")
-              Snacks.toggle.treesitter():map("<leader>uT")
-              Snacks.toggle.option("spell",
-                { name = "Spelling" }):map("<leader>us")
-              Snacks.toggle.option("wrap",
-                { name = "Wrap" }):map("<leader>uw")
-              Snacks.toggle.option("relativenumber",
-                { name = "Relative Number" }):map("<leader>uL")
-              Snacks.toggle.option("conceallevel",
-                { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
-              Snacks.toggle.option("background",
-                { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+            -- Create some toggle mappings
+            Snacks.toggle.diagnostics():map("<leader>ud")
+            Snacks.toggle.line_number():map("<leader>ul")
+            Snacks.toggle.inlay_hints():map("<leader>uh")
+            Snacks.toggle.treesitter():map("<leader>uT")
+            Snacks.toggle.option("spell",
+            { name = "Spelling" }):map("<leader>us")
+            Snacks.toggle.option("wrap",
+            { name = "Wrap" }):map("<leader>uw")
+            Snacks.toggle.option("relativenumber",
+            { name = "Relative Number" }):map("<leader>uL")
+            Snacks.toggle.option("conceallevel",
+            { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+            Snacks.toggle.option("background",
+            { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
             end
           '';
     }
