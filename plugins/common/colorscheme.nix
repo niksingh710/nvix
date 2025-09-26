11e9f50c3c71d33acaffbe215206d4413f17cc44
Nikhil Singh <nik.singh710@gmail.com>
2025-09-26 17:01:07 +0530

    undercurls: now for kitty undercurls will work

--- plugins/common/colorscheme.nix
+++ plugins/common/colorscheme.nix
@@ -4,6 +4,15 @@
     catppuccin = {
       enable = true;
       settings = {
+        integrations.native_lsp = {
+          enabled = true;
+          underlines = {
+            errors = [ "undercurl" ];
+            hints = [ "undercurl" ];
+            warnings = [ "undercurl" ];
+            information = [ "undercurl" ];
+          };
+        };
         flavor = "mocha";
         italic = true;
         bold = true;
