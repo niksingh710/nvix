152ded710a79902d7888a7883961092f6be7675f
Nikhil Singh <nik.singh710@gmail.com>
2026-05-14 13:45:59 +0530

    feat: add kanagawa theme customizations for transparent UI
    Add overrides for:
    - Floating windows (NormalFloat, FloatBorder, FloatTitle)
    - Popup menu (Pmenu, PmenuSel, PmenuSbar)
    - Blink completion menu border
    - Diagnostic virtual text with blended backgrounds
    - Tab line elements
    - Treesitter context

--- plugins/common/colorscheme.nix
+++ plugins/common/colorscheme.nix
@@ -4,10 +4,9 @@
     kanagawa = {
       enable = true;
       settings = {
-        compile = false;
+        colors.theme.all.ui.bg_gutter = "none";
         undercurl = true;
         transparent = config.nvix.transparent;
-        colors.theme.all.ui.bg_gutter = "none";
         theme = "dragon";
         background = {
           dark = "dragon";
@@ -16,6 +15,34 @@
         commentStyle.italic = true;
         keywordStyle.italic = true;
         statementStyle.bold = true;
+        overrides = # lua
+          ''
+            function(colors)
+                local theme = colors.theme
+                local makeDiagnosticColor = function(color)
+                local c = require("kanagawa.lib.color")
+                  return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+                end
+                return {
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                    Pmenu = { fg = theme.ui.shade0, bg = "none", blend = vim.o.pumblend },
+                    PmenuSel = { fg = "NONE", bg = "none" },
+                    PmenuSbar = { bg = "none" },
+                    BlinkCmpMenuBorder = { bg = "none" },
+                    DiagnosticVirtualTextHint  = makeDiagnosticColor(theme.diag.hint),
+                    DiagnosticVirtualTextInfo  = makeDiagnosticColor(theme.diag.info),
+                    DiagnosticVirtualTextWarn  = makeDiagnosticColor(theme.diag.warning),
+                    DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+                    TabLineFill = { bg = "none" },
+                    TabLine = { bg = "none" },
+                    TabLineSel = { bg = "none" },
+                    TreesitterContext = { bg ="none",  ctermbg="none" },
+                }
+            end
+          '';
       };
     };
     catppuccin = {
